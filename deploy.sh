4f9271006a15006eef92ab66b8da9ac94ce44e38
Gon_zillA <gonzaloventura@outlook.com>
2019-08-22 12:35:09 +0200

    fix setup

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+rm -rf node_modules css js
 npm install 
-ln -s node_modules/bootstrap/dist/* .
-ln -s node_modules/jquery/dist/jquery.min.js .
+mkdir css
+mkdir js
+ln -s ../node_modules/bootstrap/dist/css/bootstrap.min.css css/.
+ln -s ../node_modules/bootstrap/dist/js/bootstrap.min.js js/.
+ln -s ../node_modules/jquery/dist/jquery.min.js js/.
+ln -s ../node_modules/popper.js/dist/popper.min.js js/.
